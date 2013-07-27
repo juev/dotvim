daf3eadcf6e74c17b60be08a5a0403dcbaa0f1bc
Evsyukov Denis <denis@evsyukov.org>
2013-07-27 13:08:50 +0400

    Install only first run, and add update

--- install.sh
+++ install.sh
@@ -2,17 +2,18 @@
 if ! [ -d ~/.vim ]; then
   git clone https://github.com/Juev/dotvim.git ~/.vim
   git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
+  if ! [ -f ~/.vimrc ]; then
+    ln -s ~/.vim/vimrc ~/.vimrc
+  else
+    echo "File ~/.vimrc already exist. Skipping."
+  fi
+  if ! [ -f ~/.gvimrc ]; then
+    ln -s ~/.vim/gvimrc ~/.gvimrc
+  else
+    echo "File ~/.gvimrc already exist. Skipping."
+  fi
+  vim +BundleInstall +qall 2>/dev/null
 else
   echo "Directory ~/.vim already exist. Skipping."
+  vim +BundleUpdate +qall 2>/dev/null
 fi
-if ! [ -f ~/.vimrc ]; then
-  ln -s ~/.vim/vimrc ~/.vimrc
-else
-  echo "File ~/.vimrc already exist. Skipping."
-fi
-if ! [ -f ~/.gvimrc ]; then
-  ln -s ~/.vim/gvimrc ~/.gvimrc
-else
-  echo "File ~/.gvimrc already exist. Skipping."
-fi
-vim +BundleInstall +qall 2>/dev/null
