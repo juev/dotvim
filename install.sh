6feb8a7f13dfebbd524da1890c9fd56cdc9dd674
Evsyukov Denis <denis@evsyukov.org>
2013-07-27 13:48:06 +0400

    Adding info message

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 if ! [ -d ~/.vim ]; then
+  echo "Create config directory"
   git clone https://github.com/Juev/dotvim.git ~/.vim
   git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
   if ! [ -f ~/.vimrc ]; then
+    echo "Create symlink to ~/.vimrc"
     ln -s ~/.vim/vimrc ~/.vimrc
   else
     echo "File ~/.vimrc already exist. Skipping."
   fi
   if ! [ -f ~/.gvimrc ]; then
+    echo "Create symlink to ~/.gvimrc"
     ln -s ~/.vim/gvimrc ~/.gvimrc
   else
     echo "File ~/.gvimrc already exist. Skipping."
   fi
+  echo "Install vim plugins"
   vim +BundleInstall +qall 2>/dev/null
 else
   echo "Directory ~/.vim already exist. Skipping."
